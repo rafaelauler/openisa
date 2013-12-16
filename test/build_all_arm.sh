ff266e4fa653754fdb08b60af1f9a0267d2e52c2
Rafael Auler <auler@ic.unicamp.br>
2013-12-16 19:27:12 -0200

    ARM compilation script (shootout)

---
+++ test/build_all_arm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d | cut -c 3-); do
+    if [ $dir == "calc" ]; then
+        continue
+    fi
+    if [ $dir == "micro" ]; then
+        continue
+    fi
+    if [ $dir == "hello-world" ]; then
+        continue
+    fi
+    if [ $dir == "moments" ]; then
+        continue
+    fi
+    cd $dir
+    make clean
+    SBTOPT="-optimize" ARCH="arm" MATTR="-mattr=vfp3,d16,a8,-neon -mcpu=cortex-a8 -float-abi=hard" make
+    arm-linux-musleabihf-gcc -O3 -static ${dir}.c -o ${dir}-nat-arm
+    cd ..
+done
