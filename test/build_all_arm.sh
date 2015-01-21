4e2106058e1afbe22afbbc9c0f2f9ceef05e8867
Rafael Auler <rafaelauler@gmail.com>
2015-01-21 07:36:13 -0800

    Updating build arm script to ignore one extra folder

--- test/build_all_arm.sh
+++ test/build_all_arm.sh
@@ -10,6 +10,9 @@ for dir in $(find . -maxdepth 1 -mindepth 1 -type d | cut -c 3-); do
     if [ $dir == "hello-world" ]; then
         continue
     fi
+    if [ $dir == "testes-arm" ]; then
+        continue
+    fi
     if [ $dir == "moments" ]; then
         continue
     fi
@@ -17,7 +20,7 @@ for dir in $(find . -maxdepth 1 -mindepth 1 -type d | cut -c 3-); do
     make clean
     SBTOPT="-optimize" ARCH="arm" MATTR="-mattr=vfp3,d16,a8,-neon -mcpu=cortex-a8 -float-abi=hard" make
     if [ $? != 0 ]; then
-        echo Stopping script
+        echo Stopping script at $dir
         exit
     fi
     #arm-linux-musleabihf-gcc -O3 -static ${dir}.c -o ${dir}-nat-arm
