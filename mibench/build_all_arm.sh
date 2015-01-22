8347439baefd49548e5a611bfc6edb064fd8d039
Rafael Auler <rafaelauler@gmail.com>
2015-01-21 16:51:40 -0800

    Adapting build ARM script to work with Mibench

--- mibench/build_all_arm.sh
+++ mibench/build_all_arm.sh
@@ -1,34 +1,63 @@
 #!/bin/bash
 
-for dir in $(find . -maxdepth 1 -mindepth 1 -type d | cut -c 3-); do
-    if [ $dir == "calc" ]; then
+ARMDIR=$(pwd)/testes-arm
+
+DIRS=(automotive/basicmath
+automotive/susan
+automotive/susan
+automotive/susan
+network/patricia
+network/dijkstra
+security/rijndael
+security/rijndael
+telecomm/FFT
+telecomm/FFT)
+ACTIVATE=(yes #basicmath
+yes #susan-smoothing
+yes #susan-edges
+yes #susan-corners
+yes #patricia
+yes #dijkstra
+yes #rijndael-encode
+yes #rijndael-decode
+yes #fft
+yes) #fft-inv
+NAMES=(basicmath_large
+susan-smoothing
+susan-edges
+susan-corners
+patricia
+dijkstra
+rijndael-encode
+rijndael-decode
+fft
+fft-inv)
+ROOT=$(pwd)
+
+for index in ${!DIRS[*]}; do
+    dir=${DIRS[index]}
+    name=${NAMES[index]}
+    active=${ACTIVATE[index]}
+    if [ $active == "no" ]; then
+        echo "Skipping $name"
         continue
     fi
-    if [ $dir == "micro" ]; then
-        continue
-    fi
-    if [ $dir == "hello-world" ]; then
-        continue
-    fi
-    if [ $dir == "testes-arm" ]; then
-        continue
-    fi
-    if [ $dir == "moments" ]; then
-        continue
-    fi
+
     cd $dir
     for opts in "-oneregion" "-nolocals" "-debug-ir"; do
-	make clean
+	ARCH="arm" make clean
+	ARCH="x86" make clean
 	SBTOPT="-optimize "${opts} ARCH="arm" MATTR="-mattr=vfp3,d16,a8,-neon -mcpu=cortex-a9 -float-abi=hard" make
 	if [ $? != 0 ]; then
             echo Stopping script at $dir
             exit
 	fi
-	cp ${dir}-nat-arm ../testes-arm/binarios
+	mkdir -p ${ARMDIR}/bin
+	cp ${name}-nat-arm ${ARMDIR}/bin
 	if [ $opts == "-debug-ir" ]; then
-	    cp ${dir}-oi-arm ../testes-arm/binarios/${dir}-locals
+	    cp ${name}-oi-arm ${ARMDIR}/bin/${name}-locals
 	else
-	    cp ${dir}-oi-arm ../testes-arm/binarios/${dir}${opts}
+	    cp ${name}-oi-arm ${ARMDIR}/bin/${name}${opts}
 	fi	
 	if [ $? != 0 ]; then
             echo Stopping script at $dir
@@ -36,5 +65,5 @@ for dir in $(find . -maxdepth 1 -mindepth 1 -type d | cut -c 3-); do
 	fi	
 	#arm-linux-musleabihf-gcc -O3 -static ${dir}.c -o ${dir}-nat-arm
     done
-    cd ..
+    cd ${ROOT}
 done
