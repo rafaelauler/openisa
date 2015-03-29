5b84062e47ad6ab0ad92f8cc1c51a4ed038031fa
Rafael Auler <rafaelauler@gmail.com>
2015-03-28 18:40:31 -0700

    Adding a check_all.sh script to check infrastructure sanity and testcases for tests/micro

---
+++ test/check_all.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+GSED=sed
+LOGFILE=$(pwd)/log-check.txt
+ROOTDIR=$(pwd)
+VERBOSE=false
+
+echo Checking started. Today is $(date). | tee -a $LOGFILE
+
+for dir in $(find . -maxdepth 2 -mindepth 1 -type d | cut -c 3-); do
+    if [ $dir == "calc" ]; then
+        echo Skipping calc folder
+        continue
+    fi
+    if [[ $dir == testes-arm* ]]; then
+        echo Skipping testes-arm folder
+        continue
+    fi
+    if [ $dir == "hello-world" ]; then
+        echo Skipping hello-world folder
+        continue
+    fi
+    if [ $dir == "moments" ]; then
+        echo Skipping moments folder
+        continue
+    fi
+    cd $dir
+    for opts in "-oneregion" "-nolocals" "-debug-ir"; do
+        myopts=$opts" -optimize"
+        make clean &> /dev/null
+        echo -ne "$dir [ $myopts ] : \t\t" | tee -a $LOGFILE
+        if [ x"$VERBOSE" == x"false" ]; then
+            SBTOPT="$myopts" make &> /dev/null
+            OUT=$?
+        else
+            SBTOPT="$myopts" make
+            OUT=$?
+        fi
+        if [ $OUT -ne 0 ]; then
+            echo "BUILD FAIL!\n Program: $dir Options: $myopts \n" | tee -a $LOGFILE
+            if [ x"$VERBOSE" == x"false" ]; then
+                echo Running make verbose:
+                SBTOPT="$myopts" make       
+            fi
+            cd ..
+            exit
+        fi
+	file=${dir}
+    	if [[ $dir == micro* ]]; then
+        	file=simple
+	fi
+	if [ -e testcase.txt ]; then
+	        ./${file}-nat-x86 > out-golden.txt < testcase.txt
+	        ./${file}-oi-x86 > out-oi.txt < testcase.txt
+	else
+		./${file}-nat-x86 > out-golden.txt
+        	./${file}-oi-x86 > out-oi.txt
+	fi
+        diff out-golden.txt out-oi.txt
+        if [ $? -ne 0 ]; then
+            echo "FAIL!" | tee -a $LOGFILE
+	else
+            echo "PASS!" | tee -a $LOGFILE
+        fi
+        rm out-golden.txt out-oi.txt
+    done;
+    cd ${ROOTDIR}
+done
