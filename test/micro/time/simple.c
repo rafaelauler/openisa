aaa5754c6a92dea2ee0009e6e152a3efee77c044
Rafael Auler <rafaelauler@gmail.com>
2015-03-29 19:24:12 -0700

    Expanding time() testcase to include difftime()

--- test/micro/time/simple.c
+++ test/micro/time/simple.c
@@ -3,5 +3,10 @@
 
 int
 main() {
-  printf("time(0) = %d\n", time(0));
+  char c;
+  time_t time1 = time(0);
+  printf("time(0) = %d\n", time1);
+  scanf("%c", &c);
+  double theDiff = difftime(time(0), time1);
+  printf("%d difftime = %lf\n", 0, theDiff);
 }
