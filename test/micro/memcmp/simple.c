24d580ff1a61ff4fa05b1b1a35a27dae717e0978
Rafael Auler <rafaelauler@gmail.com>
2015-05-13 16:55:05 -0300

    Fixing memcmp testcase that was optimized out in its previous version

--- test/micro/memcmp/simple.c
+++ test/micro/memcmp/simple.c
@@ -3,9 +3,13 @@
 
 int
 main(int argc, char **argv) {
-  char *region1 = "blablablalbkreljdklafjds;kflj";
-  char *region2 = "blablablalkjs;lkfjas;dljfa;js";
-  char *region3 = "blablablalbkreljdklafjds;kflj";
+  char region1[256], region2[256], region3[256];
+  printf("Type string to fill region1\n");
+  scanf("%s", region1);
+  printf("Type string to fill region2\n");
+  scanf("%s", region2);
+  printf("Type string to fill region3\n");
+  scanf("%s", region3);
   printf("memcmp(region1, region2, 29) = %d\nmemcmp(region1, region3, 29) = %d\n",
          memcmp(region1, region2, 29),
          memcmp(region1, region3, 29));
