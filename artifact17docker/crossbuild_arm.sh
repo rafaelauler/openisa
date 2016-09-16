f4b92ef315a1199ce8d5887a1a6b13b18b60dc6e
Rafael Auler <rafaelauler@gmail.com>
2016-09-16 13:45:49 -0300

    [artifact17] Fix file name in arm build script

--- artifact17docker/crossbuild_arm.sh
+++ artifact17docker/crossbuild_arm.sh
@@ -19,7 +19,7 @@ cd ${ROOT}/openisa/spec || check_error
 ./build_all_arm.sh || check_error
 cd ${ROOT}/openisa/spec/testes-arm || check_error
 cp -v ${ROOT}/openisa/spec/testes-x86/*.gnuplot ${ROOT}/openisa/spec/testes-arm || check_error
-tar cjvf bundle-arm.tar.bz2 bin input measure-arm.sh *.gnuplot || check_error
+tar cjvf bundle-arm.tar.bz2 bin input measure_arm.sh *.gnuplot || check_error
 cp -v bundle-arm.tar.bz2 ${ROOT}/.. || check_error
 
 echo "Finished. Please deploy bundle-arm.tar.bz2 to an ARM Linux platform and run measure-arm.sh"
